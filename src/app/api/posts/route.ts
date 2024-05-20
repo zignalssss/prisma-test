bdfdfe6bbf01f432f37e3769f9ae917321dc1e1d
Wiritipon Thapkhan <81100736+zignalssss@users.noreply.github.com>
2024-05-21 00:04:40 +0700

    Add files via upload

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+export async function GET(){
+    return Response.json(await prisma.post.findMany())
+}
+
+export async function POST(req: Request) {
+    try {
+      const { title, content } = await req.json()
+      const newPost = await prisma.post.create({
+        data: {
+          title,
+          content,
+        },
+      })
+      return Response.json(newPost)
+    } catch (error) {
+      return new Response(error as BodyInit, {
+        status: 500,
+      })
+    }
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-black flex justify-center">
+      <div className="text-white">TEST</div>
+    </div>
+  )
+}
